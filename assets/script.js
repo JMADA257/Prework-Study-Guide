cdb21ab043444d001dc344a59f2396b92f9f9184
JMADA257 <jadams1812@hotmail.com>
2023-08-05 12:50:56 -0600

    added basic javascript console codes

---
+++ assets/script.js
@@ -0,0 +1,19 @@
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+
+for (var x = 0; x < topics.length; x++) {
+ console.log(topics[x]);
+}
+
+/*
+if (topic === 'HTML') {
+ console.log("Let's study HTML!");
+} else if (topic === 'CSS') {
+ console.log("Let's study CSS!");
+} else if (topic === 'Git') {
+ console.log("Let's study Git!");
+} else if (topic === 'JavaScript') {
+ console.log("Let's study JavaScript!");
+} else {
+ console.log('Please try again!');
+}
+*/
